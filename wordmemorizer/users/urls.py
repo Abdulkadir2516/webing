49bdbccaad4c8b51524e6826c619d81ef3bc2428
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-06-14 15:07:05 +0300

    first commit

---
+++ wordmemorizer/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import register_view, home
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('', LoginView.as_view(template_name='login.html'), name='login'),  # root URL login sayfası
+    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),  # istersen bunu kaldırabilirsin, kök zaten login yapıyor
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('home/', home, name='home'),
+
+]
---
+++ wordmemorizer/users/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages  # hata mesajları için
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def home(request):
+    # Örnek değerler, gerçek veritabanından çekebilirsin
+    daily_goal = 20
+    repeated_words_today = 12
+    repeat_data = [5, 8, 10, 7, 12, 15, 12]  # Hafta içi günlük tekrarlar
+
+    context = {
+        'daily_goal': daily_goal,
+        'repeated_words_today': repeated_words_today,
+        'repeat_data': repeat_data,
+    }
+    return render(request, 'home.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Kullanıcı adı veya şifre yanlış')
+    return render(request, 'login.html')
